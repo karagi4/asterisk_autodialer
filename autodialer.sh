380d377a739134220a7a2b17cce2f4e72b273fa2
Aleksey <karagi4@yandex.ru>
2019-10-23 13:03:51 +0300

    "0.42(23.10.2019)"

--- autodialer.sh
+++ autodialer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-AUTODIALER_VERSION_FULL="0.41(19.09.2019)"
+AUTODIALER_VERSION_FULL="0.42(23.10.2019)"
 AUTODIALER_VERSION=$(echo $AUTODIALER_VERSION_FULL |awk 'BEGIN {FS="("} {print $1}')
 DATE=$(date +%Y%m%d%H%M)
 Path=$(dirname $0)
@@ -36,6 +36,7 @@ campy () {
 
     sqlread="select concat(number,',',camp) from autodialer.$campaign where status in ('$answer_status', 'NOANS', 'BUSY', 'NOANSWER', 'CONGESTION') order by RAND() limit $limit"
     RES=`mysql -h127.0.0.1 -u $db_user -p$db_pass --skip-column-names --default-character-set=utf8 $db -e "$sqlread"`
+    RES=$(echo $RES |sed 's#\ ##g')
 
     printf "$RES" >> $d
     echo "" >> $d
@@ -164,4 +165,3 @@ do
         campy "$i" "$retry" "$pause" "$timeout" "$concurrent" "$limit" "$dest" "$chcon" "$extcon" "" "$callerid" "" "$callerid_name" "$Account"
     fi
 done
-
